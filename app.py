2b97c57ffe9624a6285a0f5728ab132b18f244ac
jephdev <engr.jeffmanzanillo@gmail.com>
2025-01-07 13:22:41 +0800

    output January 2025 only

--- app.py
+++ app.py
@@ -38,17 +38,6 @@ def main():
         logging.info("📝 Timesheet collection started...")
 
         sheet_names = ["202501", 
-                       "202502",
-                       "202503",
-                       "202504",
-                       "202505",
-                       "202506",
-                       "202507",
-                       "202508",
-                       "202509",
-                       "202510",
-                       "202511",
-                       "202512"
                        ]
 
         for sheet_name in sheet_names:

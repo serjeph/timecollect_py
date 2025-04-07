974ca50de1c025abaf971ab2df7f58298dd75f17
jephdev <engr.jeffmanzanillo@gmail.com>
2025-04-07 12:05:24 +0800

    updated ranges

--- app.py
+++ app.py
@@ -38,8 +38,8 @@ def main():
         )
         logging.info("📝 Timesheet collection started...")
 
-        sheet_names = ["202503", 
-                       "202504"
+        sheet_names = ["202504", 
+                       "202505"
                        ]
 
         for sheet_name in sheet_names:
